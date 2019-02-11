f792ae8131e2cc896da772e5eff39c2f919467cb
sumitgupta2111 <sumitkumarg@hotmail.com>
2019-02-11 23:38:47 +0530

    Updates

---
+++ precache-manifest.e4d162f4ae8fd5fe98581e14182fd5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "eea897da25d4f04b7819",
+    "url": "/react-app/static/js/main.eea897da.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-app/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "eea897da25d4f04b7819",
+    "url": "/react-app/static/css/main.83ff375e.chunk.css"
+  },
+  {
+    "revision": "8721d5cd709286b08c5b21f974e5b8c3",
+    "url": "/react-app/index.html"
+  }
+];
